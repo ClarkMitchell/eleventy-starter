723e1f16bc2cffe60dcb953781b75ee3eb94aca0
ClarkMitchell <clarkmitchelldev@gmail.com>
2020-12-01 22:22:35 -0500

    remove redundant css purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,30 +1,10 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-
-const plugins = [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('postcss-100vh-fix'),
-    require('cssnano')({
-        preset: 'default',
-    }),
-];
-
-if (process.env.NODE_ENV === 'production') {
-    plugins.push(
-        purgecss({
-            content: ['./dist/**/*.html'],
-            extractors: [
-                {
-                    extractor: class TailwindExtractor {
-                        static extract(content) {
-                            return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-                        }
-                    },
-                    extensions: ['css', 'html', 'vue'],
-                },
-            ],
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('postcss-100vh-fix'),
+        require('cssnano')({
+            preset: 'default',
         }),
-    );
-}
-
-module.exports = { plugins };
+    ],
+};
